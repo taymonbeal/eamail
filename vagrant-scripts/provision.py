eef6bea15f5e504b6e625f8ec2918261ebf48312
Taymon A. Beal <taymonbeal@gmail.com>
2017-01-10 20:38:08 -0500

    Update provision.py to stop using emailauth

--- vagrant-scripts/provision.py
+++ vagrant-scripts/provision.py
@@ -3,10 +3,9 @@ from __future__ import unicode_literals
 from django import setup
 setup()
 
+from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
-
-from project.emailauth.models import User
 
 
 Site.objects.update(domain='localhost:8000', name='EA Boston Newsletter')
-User.objects.create_superuser(email='admin@localhost', password='admin')
+User.objects.create_superuser(username='admin', password='admin', email=None)
