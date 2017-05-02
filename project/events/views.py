c40b88a97b08a86706127eb07d86ce845ba721d7
Robin Tanenbaum <robintanenbaum@gmail.com>
2017-05-01 23:49:45 -0400

    stopped main page from displaying past events

--- project/events/views.py
+++ project/events/views.py
@@ -3,8 +3,9 @@ from __future__ import unicode_literals
 from django.views.generic.list import ListView
 
 from .models import Event
+from django.db.models.functions import Now
 
 
 class EventListView(ListView):
     model = Event
-    queryset = Event.objects.order_by('start_time')
+    queryset = Event.objects.filter(start_time__gte=Now()).order_by('start_time')
