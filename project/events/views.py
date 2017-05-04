3f11f638e6fe074508f505a455a636b01f925f10
Robin Tanenbaum <robintanenbaum+github@gmail.com>
2017-05-04 12:52:28 -0400

    Sorts events chronologically (#9)
    * using flexboxes to tile events, with event pics as header backgrounds
    
    * made more text overlap image, removed footer
    
    * sorted events by start time
    
    * stopped main page from displaying past events
    
    * created a separate past events page
    
    * shortened order-reversal code in past view
    
    * responded to code review
    
    * alphabetically sorted imports

--- project/events/views.py
+++ project/events/views.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from django.db.models.functions import Now
 from django.views.generic.list import ListView
 
 from .models import Event
@@ -7,3 +8,10 @@ from .models import Event
 
 class EventListView(ListView):
     model = Event
+    queryset = Event.objects.filter(start_time__gte=Now()).order_by('start_time')
+
+
+class PastEventListView(ListView):
+    model = Event
+    queryset = Event.objects.filter(start_time__lte=Now()).order_by('-start_time')
+    template_name = 'events/event_list.html'
