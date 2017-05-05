a24fc34b3af0f076185a65ccec800a11460eb827
Robin Tanenbaum <robintanenbaum+github@gmail.com>
2017-05-04 22:35:22 -0400

    Adds permalink pages (#10)
    * Basic permalink functionality set up
    
    * made stylesheet apply properly to permalink pages
    
    * made permalink pages show category list
    
    * named past events url
    
    * adding navigation options
    
    * formatted header link
    
    * added nav links to details template

--- project/events/views.py
+++ project/events/views.py
@@ -1,9 +1,13 @@
 from __future__ import unicode_literals
 
 from django.db.models.functions import Now
+from django.views.generic import DetailView
 from django.views.generic.list import ListView
 
 from .models import Event
+
+class EventDetailView(DetailView):
+    model = Event
 
 
 class EventListView(ListView):
