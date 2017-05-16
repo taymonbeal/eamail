388171a8cfcf2d71d92da79144f3cb3a48e53cbe
Robin Tanenbaum <robintanenbaum@gmail.com>
2017-05-16 19:14:23 -0400

    adding a view for the new subscriber form

--- project/subscriptions/views.py
+++ project/subscriptions/views.py
@@ -1,9 +1,14 @@
 from __future__ import unicode_literals
 
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import FormView, UpdateView
 
+from .forms import NewSubscriberForm
 from .viewmixins import SubscriberMixin
 
 
 class SubscriberUpdateView(SubscriberMixin, UpdateView):
     fields = ['interests']
+
+
+class NewSubscriptionView(FormView):
+    form_class = NewSubscriberForm
