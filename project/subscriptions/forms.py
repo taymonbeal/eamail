95e2d07794be23a573ac10289b322a875f5bf44a
Robin Tanenbaum <robintanenbaum@gmail.com>
2017-05-21 17:39:25 -0500

    made form interests be checkbox list

--- project/subscriptions/forms.py
+++ project/subscriptions/forms.py
@@ -7,9 +7,4 @@ class NewSubscriberForm(forms.ModelForm):
     class Meta:
         model = Subscriber
         fields = ('email', 'interests')
-
-    def save(self, *args, **kwargs):
-        if self.token is None:
-            self.token = Token()
-            self.token.save()
-        super(Subscriber, self).save(*args, **kwargs)
+        widgets = { 'interests': forms.CheckboxSelectMultiple }
