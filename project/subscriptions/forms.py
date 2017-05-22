d054348b599b552012c472201e9b7eb60a7364a9
Robin Tanenbaum <robintanenbaum+github@gmail.com>
2017-05-22 14:42:06 -0500

    Subscription form (#14)
    * rough implementation of non-absolute end dates
    
    * moved event list away from homepage
    
    * drafting new subscriber form
    
    * adding a view for the new subscriber form
    
    * adding basic new subscriber form
    
    * moved header and stylesheet from specific templates to base template
    
    * styling forms
    
    * adding new subscriber to database when form is valid
    
    * made form interests be checkbox list
    
    * made tokens be created automatically for new subscribers
    
    * Implemented post-subscription redirect and success message
    
    * removed spaces from inside braces

---
+++ project/subscriptions/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from .models import Subscriber
+
+class NewSubscriberForm(forms.ModelForm):
+
+    class Meta:
+        model = Subscriber
+        fields = ('email', 'interests')
+        widgets = {'interests': forms.CheckboxSelectMultiple}
--- project/subscriptions/views.py
+++ project/subscriptions/views.py
@@ -1,9 +1,23 @@
 from __future__ import unicode_literals
 
-from django.views.generic.edit import UpdateView
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.views.generic.edit import FormView, UpdateView
 
+from .forms import NewSubscriberForm
 from .viewmixins import SubscriberMixin
 
 
 class SubscriberUpdateView(SubscriberMixin, UpdateView):
     fields = ['interests']
+
+
+class NewSubscriptionView(FormView):
+    form_class = NewSubscriberForm
+    template_name = 'subscriptions/new_subscription.html'
+    success_url = '/future/'
+
+    def form_valid(self, form):
+        form.save()
+        messages.success(self.request, 'Thank you for subscribing.')
+        return redirect('event_list')
