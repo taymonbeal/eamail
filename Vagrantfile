97d113649295c3384c0b570281b34f60550bfe40
Taymon A. Beal <taymonbeal@gmail.com>
2017-05-23 08:49:45 -0400

    Create sendnewsletter management command (#16)

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,7 @@
 Vagrant.configure(2) do |config|
   config.vm.box = 'ubuntu/xenial64'
+  config.vm.network :forwarded_port, guest: 1080, host: 1080
   config.vm.network :forwarded_port, guest: 8000, host: 8000
   config.vm.provision :shell, path: 'vagrant-scripts/provision.sh', privileged: false
+  config.vm.provision :shell, path: 'vagrant-scripts/startup.sh', privileged: false, run: :always
 end
--- vagrant-scripts/provision.sh
+++ vagrant-scripts/provision.sh
@@ -2,8 +2,9 @@
 set -o errexit
 set -o pipefail
 sudo apt-get --assume-yes update
-sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes install build-essential libbz2-dev libmysqlclient-dev libreadline-dev libsqlite3-dev libssl-dev mysql-server-5.7
+sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes install build-essential libbz2-dev libmysqlclient-dev libreadline-dev libsqlite3-dev libssl-dev mysql-server-5.7 ruby-dev
 sudo apt-get --assume-yes autoremove
+sudo gem install mailcatcher
 wget --output-document=- --progress=bar:force https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 ~/.pyenv/bin/pyenv install --verbose 2.7.12
 ~/.pyenv/versions/2.7.12/bin/pip install --upgrade pip
