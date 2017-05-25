359e9402d630d2cb8070126cdf2c27eb3c4d810c
Taymon A. Beal <taymonbeal@gmail.com>
2017-05-25 08:34:18 -0400

    Filter newsletter contents based on time

--- project/subscriptions/management/commands/sendnewsletter.py
+++ project/subscriptions/management/commands/sendnewsletter.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 
 from django.core.mail import get_connection, send_mail
 from django.core.management.base import BaseCommand
+from django.db.models.functions import Now, TruncDate
 from django.template.loader import render_to_string
 
 from ...models import Subscriber
@@ -18,7 +19,9 @@ class Command(BaseCommand):
         with get_connection() as connection:
             for subscriber in Subscriber.objects.all():
                 events = Event.objects.filter(
-                    interests__subscriber=subscriber
+                    interests__subscriber=subscriber,
+                    start_time__date__gte=TruncDate(Now()),
+                    start_time__date__lt=(TruncDate(Now()) + timedelta(days=7)),
                 ).distinct().order_by('start_time', 'name')
                 if events:
                     send_mail(
--- vagrant-scripts/provision.sh
+++ vagrant-scripts/provision.sh
@@ -6,12 +6,14 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes install build-essential
 sudo apt-get --assume-yes autoremove
 sudo gem install mailcatcher
 wget --output-document=- --progress=bar:force https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-~/.pyenv/bin/pyenv install --verbose 2.7.12
-~/.pyenv/versions/2.7.12/bin/pip install --upgrade pip
-~/.pyenv/versions/2.7.12/bin/pip install virtualenv
-~/.pyenv/versions/2.7.12/bin/virtualenv ~/venv
+~/.pyenv/bin/pyenv install --verbose 2.7.13
+~/.pyenv/versions/2.7.13/bin/pip install --upgrade pip
+~/.pyenv/versions/2.7.13/bin/pip install virtualenv
+~/.pyenv/versions/2.7.13/bin/virtualenv ~/venv
 source /vagrant/vagrant-scripts/session.sh
 pip install --requirement=/vagrant/requirements.txt
+mysql_tzinfo_to_sql ~/venv/lib/python2.7/site-packages/pytz/zoneinfo | sudo mysql mysql
+sudo service mysql restart
 sudo mysql </vagrant/vagrant-scripts/provision.sql
 django-admin migrate --no-input
 python /vagrant/vagrant-scripts/provision.py
